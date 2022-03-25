3db8bad5622b71102b722bbdf8292dcd709fe8be
Markilha <markilha@gmail.com>
2022-03-24 22:54:00 -0300

    busca de animes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import qs from 'qs';
+import SearchInput from './SearchInput';
+import Pagination from './Pagination';
+import './styles.css';
 
-function App() {
+const api = 'https://kitsu.io/api/edge/';
+
+const LIMIT = 12;
+
+export default function App() {
+  const [info, setInfo] = useState({});
+  const [text, setText] = useState('');
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    setInfo({});
+
+    const query = {
+      page: {
+        limit: LIMIT,
+        offset
+      }
+    };
+
+    if (text) {
+      query.filter = {
+        text
+      };
+    }
+
+    fetch(`${api}anime?${qs.stringify(query)}`)
+      .then((response) => response.json())
+      .then((response) => {
+        setInfo(response);
+      });
+  }, [text, offset]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animes</h1>
+      <SearchInput
+        value={text}
+        onChange={(search) => setText(search)}
+      />
+      {text && !info.data && <span>Carregando...</span>}
+      {info.data && (
+        <ul className="animes-list">
+          {info.data.map((anime) => (
+            <li key={anime.id}>
+              <img
+                src={anime.attributes.posterImage.small}
+                alt={anime.attributes.canonicalTitle}
+              />
+              {anime.attributes.canonicalTitle}
+            </li>
+          ))}
+        </ul>
+      )}
+      {info.meta && (
+        <Pagination
+          limit={LIMIT}
+          total={info.meta.count}
+          offset={offset}
+          setOffset={setOffset}
+        />
+      )}
     </div>
   );
 }
-
-export default App;
